ea8737d5fe5f7d1a6199c09e46316126353b22be
daksh03 <dakshchoudhary.1234@gmail.com>
2025-05-12 15:58:14 +0530

    Connecting using FEIGN

---
+++ src/main/java/com/example/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.example.currency_conversion_service.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.currency_conversion_service.entity.CurrencyConversion;
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+	@PathVariable String from,
+	@PathVariable String to) ;
+
+}
