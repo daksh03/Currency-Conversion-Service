474c4f19c4b667c859b681ab8ce6db3beafcae67
daksh03 <dakshchoudhary.1234@gmail.com>
2025-05-13 16:47:45 +0530

    Connecting using eureka-netflix

--- src/main/java/com/example/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/example/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.currency_conversion_service.entity.CurrencyConversion;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
